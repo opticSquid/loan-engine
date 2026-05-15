e15107d001b9d9ed407b05f1a9641daf33386b4f
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2026-05-15 15:12:20 +0530

    Initial Commit

---
+++ src/main/java/com/gullak/loan_engine/LoanEngineApplication.java
@@ -0,0 +1,13 @@
+package com.gullak.loan_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gullak/loan_engine/LoanEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gullak.loan_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
